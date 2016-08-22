cec2d763025baacf57c789ace548c715325125e8
Rob Reynolds <ferventcoder@gmail.com>
2016-08-22 10:09:36 -0500

    Add 1password and git.install for example packages

---
+++ automatic/1password/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= '1password' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = '{{DownloadUrl}}'
+  softwareName  = '1Password*'
+  checksum      = '{{Checksum}}'
+  checksumType  = 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
