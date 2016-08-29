61d4ad9194049fcff8a97c6cc2a4b81b8ef9e243
Mike Cole <cole.mike@gmail.com>
2016-08-29 16:40:45 -0500

    Added screenhero.

---
+++ manual/screenhero/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$exeFile = "Screenhero-Latest-setup.exe"
+
+$packageArgs = @{
+  packageName   = 'screenhero'
+  unzipLocation = "$unzipLocation"
+  fileType      = 'exe'
+  url           = "$(Join-Path $unzipLocation $exeFile)"
+  silentArgs    = '/Q'
+  validExitCodes= @(0)
+  softwareName  = 'screenhero*'
+}
+
+Install-ChocolateyPackage @packageArgs
