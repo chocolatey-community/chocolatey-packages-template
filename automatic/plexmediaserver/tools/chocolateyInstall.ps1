0f404e7ac4e96361da3d582a2354684d49e90365
Mike Cole <cole.mike@gmail.com>
2016-09-16 22:52:35 -0500

    Working on plexmediaserver AU

---
+++ automatic/plexmediaserver/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$url = 'https://downloads.plex.tv/plex-media-server/1.0.3.2461-35f0caa/Plex-Media-Server-1.0.3.2461-35f0caa-en-US.exe'
+$checksum = 'B31465A25C252B4B52019077BFCEA96B5D379508CAACB045F8762712B9193A25'
+
+$packageArgs = @{
+  packageName   = 'plexmediaserver'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = $url
+  silentArgs    = '/quiet'
+  validExitCodes= @(0)
+  softwareName  = 'plexmediaserver*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/plexmediaserver/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$homepage = 'http://storageexplorer.com'
+$url = 'http://go.microsoft.com/fwlink/?LinkId=708343'
+$filename = 'StorageExplorer.exe'
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $homepage_content = Invoke-WebRequest -Uri $homepage
+
+    # Get Version
+    $homepage_content -match '(Version \d+.\d+.\d+)'
+    $version = $matches[1] -replace "Version ", ""
+
+    # Get Checksum
+    $temp_file = $env:TEMP + '\' + $filename
+    Invoke-WebRequest $url -OutFile $temp_file
+    $checksum = $(Get-FileHash $temp_file -Algorithm SHA256 | Select-Object -ExpandProperty Hash)
+
+    $Latest = @{ Checksum = $checksum; Version = $version }
+    Write-Host $Latest
+    return $Latest
+}
+
+update -ChecksumFor 32
