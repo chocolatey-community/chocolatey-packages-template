8671d5326beba882cc9e1918011b662aae8e83e5
Mike Cole <cole.mike@gmail.com>
2016-09-18 18:35:38 -0500

    Adjusted update script.

--- automatic/microsoftazurestorageexplorer/update.ps1
+++ automatic/microsoftazurestorageexplorer/update.ps1
@@ -2,32 +2,24 @@ import-module au
 
 $homepage = 'http://storageexplorer.com'
 $url = 'http://go.microsoft.com/fwlink/?LinkId=708343'
-$filename = 'StorageExplorer.exe'
 
 function global:au_SearchReplace {
     @{
         'tools\ChocolateyInstall.ps1' = @{
-            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum)'"
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
         }
      }
 }
 
 function global:au_GetLatest {
-    Write-Host 'Start'
     $homepage_content = Invoke-WebRequest -Uri $homepage -UseBasicParsing
-    Write-Host 'Context downloaded'
+
     # Get Version
     $homepage_content -match '(Version \d+.\d+.\d+)'
     $version = $matches[1] -replace "Version ", ""
 
-    # Get Checksum
-    $temp_file = $env:TEMP + '\' + $filename
-    Invoke-WebRequest $url -OutFile $temp_file
-    $checksum = $(Get-FileHash $temp_file -Algorithm SHA256 | Select-Object -ExpandProperty Hash)
-
-    $Latest = @{ Checksum = $checksum; Version = $version }
-    Write-Host $Latest
+    $Latest = @{ URL = $url; Version = $version }
     return $Latest
 }
 
-update
+update -NoCheckUrl -ChecksumFor 32
