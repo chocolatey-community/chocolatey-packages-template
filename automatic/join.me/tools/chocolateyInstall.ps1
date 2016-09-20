3857dbbfe72df962379499a24107676eb7692644
Mike Cole <cole.mike@gmail.com>
2016-09-19 19:36:40 -0500

    Adjusting join.me installer.

--- automatic/join.me/tools/chocolateyInstall.ps1
+++ automatic/join.me/tools/chocolateyInstall.ps1
@@ -1,11 +1,10 @@
-﻿$url = 'https://www.join.me/Download.aspx?installer=win'
-$checksum = '14AB9B659DBB14D88759B0F9CD39926FFF0D21E778014D1EB22FAA7C0EEAACB1'
+﻿$checksum = '14AB9B659DBB14D88759B0F9CD39926FFF0D21E778014D1EB22FAA7C0EEAACB1'
 
 $packageArgs = @{
   packageName   = 'join.me'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'msi'
-  url           = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
+  url           = 'https://www.join.me/Download.aspx?installer=win'
   silentArgs    = '/quiet'
   validExitCodes= @(0)
   softwareName  = 'join.me*'
