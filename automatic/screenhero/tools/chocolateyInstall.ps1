4c08ef2dd2c3c32cedd46d86bc0d09e57519d199
Mike Cole <cole.mike@gmail.com>
2016-09-19 23:14:36 -0500

    Working on screenhero au

---
+++ automatic/screenhero/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$checksum = '14AB9B659DBB14D88759B0F9CD39926FFF0D21E778014D1EB22FAA7C0EEAACB1'
+
+$packageArgs = @{
+  packageName   = 'screenhero'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = 'http://dl.screenhero.com/update/win/Screenhero-Latest-setup.exe'
+  silentArgs    = '/Q'
+  validExitCodes= @(0)
+  softwareName  = 'screenhero*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
