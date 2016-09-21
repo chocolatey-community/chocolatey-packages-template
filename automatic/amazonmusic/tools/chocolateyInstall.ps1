283f6e185897b51d22a9619bf9181e6a8c401a40
Mike Cole <cole.mike@gmail.com>
2016-09-20 21:29:59 -0500

    Fixed amazonmusic.

--- automatic/amazonmusic/tools/chocolateyInstall.ps1
+++ automatic/amazonmusic/tools/chocolateyInstall.ps1
@@ -1,17 +1,9 @@
 $url          = 'https://images-na.ssl-images-amazon.com/images/G/01/digital/music/morpho/installers/20160616/222132fbc5/AmazonMusicInstaller.exe'
 $checksum     = 'E908889E16E5B0C58B670BB74AB6E3F8AC48F5BEA2D59899665439FCD5504A63'
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileFullPath = "Join-Path $toolsDir 'AmazonMusicInstaller.exe'"
+$toolsDir     = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$fileFullPath = Join-Path $toolsDir 'AmazonMusicInstaller.exe'
 
-$packageArgs = @{
-  packageName    = 'amazonmusic'
-  fileFullPath   = $fileFullPath
-  url            = $url
-  checksum       = $checksum
-  checksumType   = 'sha256'
-}
-
-Get-ChocolateyWebFile $packageArgs
+Get-ChocolateyWebFile -PackageName 'amazonmusic' -FileFullPath $fileFullPath -Url $url -Checksum $checksum -ChecksumType 'sha256'
 
 $installArgs = '--unattendedmodeui none'
 start-process $fileFullPath $installArgs
