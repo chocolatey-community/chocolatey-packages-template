a67c85c133bebdb9fabb4c2765e35d3471655fc2
Mike Cole <cole.mike@gmail.com>
2016-09-20 19:16:42 -0500

    Prepping amazonmusic for au.

--- automatic/amazonmusic/tools/chocolateyInstall.ps1
+++ automatic/amazonmusic/tools/chocolateyInstall.ps1
@@ -1,15 +1,17 @@
+$url          = 'https://images-na.ssl-images-amazon.com/images/G/01/digital/music/morpho/installers/20160616/222132fbc5/AmazonMusicInstaller.exe'
+$checksum     = 'E908889E16E5B0C58B670BB74AB6E3F8AC48F5BEA2D59899665439FCD5504A63'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileFullPath = "Join-Path $toolsDir 'AmazonMusicInstaller.exe'"
 
 $packageArgs = @{
   packageName    = 'amazonmusic'
   fileFullPath   = $fileFullPath
-  url           = 'https://images-na.ssl-images-amazon.com/images/G/01/digital/music/morpho/installers/20160616/222132fbc5/AmazonMusicInstaller.exe'
-  checksum      = 'E908889E16E5B0C58B670BB74AB6E3F8AC48F5BEA2D59899665439FCD5504A63'
-  checksumType  = 'sha256'
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
 }
 
-Get-ChocolateyWebFile @packageArgs
+Get-ChocolateyWebFile $packageArgs
 
 $installArgs = '--unattendedmodeui none'
 start-process $fileFullPath $installArgs
