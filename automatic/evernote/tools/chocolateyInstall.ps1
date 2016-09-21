a2a4b186ba70ca366a686c7266bef985da7f709a
Mike Cole <cole.mike@gmail.com>
2016-09-20 21:44:42 -0500

    Preparing evernote for au

---
+++ automatic/evernote/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$checksum = '87370ca1c100461e332150673412e7befaf1bb9ecb13ef7daec6dfb1ad064bf2'
+
+$packageArgs = @{
+  packageName   = 'evernote'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = 'https://evernote.com/download/get.php?file=Win'
+  silentArgs    = '/quiet'
+  validExitCodes= @(0)
+  softwareName  = 'evernote*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
