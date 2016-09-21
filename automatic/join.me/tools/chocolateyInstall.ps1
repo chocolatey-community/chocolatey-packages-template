ee532ebeb68048f7e843026a073b50ccac89a350
Mike Cole <cole.mike@gmail.com>
2016-09-21 09:19:41 -0500

    Fixed url in join.me au.

--- automatic/join.me/tools/chocolateyInstall.ps1
+++ automatic/join.me/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName   = 'join.me'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'msi'
-  url           = 'https://www.join.me/Download.aspx'
+  url           = 'https://www.join.me/Download.aspx?installer=win&webdownload=true'
   silentArgs    = '/quiet'
   validExitCodes= @(0)
   softwareName  = 'join.me*'
