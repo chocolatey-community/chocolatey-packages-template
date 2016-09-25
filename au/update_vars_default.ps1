5e6d7b43d5b7739f81c6c742481d036bc224907c
Miodrag Milic <miodrag.milic@gmail.com>
2016-09-25 19:03:39 +0200

    changes requested by ferventcoder

--- au/update_vars_default.ps1
+++ au/update_vars_default.ps1
@@ -1,10 +1,13 @@
-# AU template: https://raw.githubusercontent.com/majkinetor/au/master/update_all_default.ps1
-#    env vars: https://raw.githubusercontent.com/majkinetor/au/master/update_vars_default.ps1
+# AU Packages Template: https://github.com/majkinetor/au-packages-template
 
 $Env:mail_user        = ''
 $Env:mail_pass        = ''
+$Env:mail_server      = 'smtp.gmail.com'
+$Env:mail_port        = '587'
+$Env:mail_enablessl   = 'true'
+
 $Env:api_key          = ''          #Chocolatey api key
-$Env:gist_id          = ''
+$Env:gist_id          = ''          #Specify your gist id or leave empty for anonymous gist
 $Env:github_user_repo = ''          #{github_user>/{repo}
-$Env:github_api_key   = ''
+$Env:github_api_key   = ''          #Github personal access token
 $Env:au_Push          = 'false'     #Push to chocolatey
