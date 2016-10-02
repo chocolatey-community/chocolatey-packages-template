1fc968bf85894a58cb08da9ed6cc57af5872350f
Mike Cole <cole.mike@gmail.com>
2016-10-01 23:28:39 -0500

    Updating dotnet452developerpack

--- manual/dotnet452developerpack/tools/chocolateyInstall.ps1
+++ manual/dotnet452developerpack/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@ $packageArgs = @{
   packageName   = 'dotnet452developerpack'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'exe'
-  url           = 'https://download.microsoft.com/download/B/4/1/B4119C11-0423-477B-80EE-7A474314B347/NDP452-KB2901954-Web.exe'
-  silentArgs    = '/q'
-  validExitCodes= @(0)
+  url           = 'https://download.microsoft.com/download/4/3/B/43B61315-B2CE-4F5B-9E32-34CCA07B2F0E/NDP452-KB2901951-x86-x64-DevPack.exe'
+  silentArgs    = '/passive /norestart'
+  validExitCodes= @(0,3010)
   softwareName  = 'dotnet452developerpack*'
-  checksum      = 'BD173D14A371E6786C4AE90BE1F2C560458D672BA4CBEB3CF55BEBFEF2E2778A'
+  checksum      = '127725B8E955E764BA08E87706D0B0DF6B52CFFF45BBA7D2B7DA09DF55D16166'
   checksumType  = 'sha256'
 }
 
