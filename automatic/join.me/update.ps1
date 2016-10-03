e89cf809d246643d57ee0bd67eee814fd4ca8fa5
Mike Cole <cole.mike@gmail.com>
2016-10-02 20:12:49 -0500

    Working on repairing join.me au.

--- automatic/join.me/update.ps1
+++ automatic/join.me/update.ps1
@@ -2,43 +2,6 @@ import-module au
 
 $url = 'https://www.join.me/Download.aspx?installer=win&webdownload=true'
 
-function Get-MsiDatabaseVersion {
-    param (
-        [IO.FileInfo] $FilePath
-    )
-
-    try {
-        $windowsInstaller = New-Object -com WindowsInstaller.Installer
-
-        $database = $windowsInstaller.GetType().InvokeMember(
-                "OpenDatabase", "InvokeMethod", $Null, 
-                $windowsInstaller, @($FilePath.FullName, 0)
-            )
-
-        $q = "SELECT Value FROM Property WHERE Property = 'ProductVersion'"
-        $View = $database.GetType().InvokeMember(
-                "OpenView", "InvokeMethod", $Null, $database, ($q)
-            )
-
-        $View.GetType().InvokeMember("Execute", "InvokeMethod", $Null, $View, $Null)
-
-        $record = $View.GetType().InvokeMember(
-                "Fetch", "InvokeMethod", $Null, $View, $Null
-            )
-
-        $productVersion = $record.GetType().InvokeMember(
-                "StringData", "GetProperty", $Null, $record, 1
-            )
-
-        $View.GetType().InvokeMember("Close", "InvokeMethod", $Null, $View, $Null)
-
-        return $productVersion
-
-    } catch {
-        throw "Failed to get MSI file version the error was: {0}." -f $_
-    }
-}
-
 function global:au_SearchReplace {
     @{
         'tools\ChocolateyInstall.ps1' = @{
@@ -54,8 +17,29 @@ function global:au_GetLatest {
     Invoke-WebRequest $url -OutFile $temp_file
     Write-Host $temp_file
 
-    $version = Get-MsiDatabaseVersion $temp_file
-    Write-Host $version
+    $windowsInstaller = New-Object -com WindowsInstaller.Installer
+
+    $database = $windowsInstaller.GetType().InvokeMember(
+            "OpenDatabase", "InvokeMethod", $Null, 
+            $windowsInstaller, @(([IO.FileInfo]$temp_file).FullName, 0)
+        )
+
+    $q = "SELECT Value FROM Property WHERE Property = 'ProductVersion'"
+    $View = $database.GetType().InvokeMember(
+            "OpenView", "InvokeMethod", $Null, $database, ($q)
+        )
+
+    $View.GetType().InvokeMember("Execute", "InvokeMethod", $Null, $View, $Null)
+
+    $record = $View.GetType().InvokeMember(
+            "Fetch", "InvokeMethod", $Null, $View, $Null
+        )
+
+    $version = $record.GetType().InvokeMember(
+            "StringData", "GetProperty", $Null, $record, 1
+        )
+
+    $View.GetType().InvokeMember("Close", "InvokeMethod", $Null, $View, $Null)
 
     $checksum = $(Get-FileHash $temp_file -Algorithm SHA256 | Select-Object -ExpandProperty Hash)
     Write-Host $checksum
