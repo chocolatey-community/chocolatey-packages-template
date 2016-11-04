3c5826b11c40c71448ec6ded25b902947d77671c
Mike Cole <cole.mike@gmail.com>
2016-11-04 08:14:40 -0500

    Adjusted stickies to use new installer.

--- manual/stickies/tools/chocolateyInstall.ps1
+++ manual/stickies/tools/chocolateyInstall.ps1
@@ -1,18 +1,13 @@
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileFullPath = Join-Path $toolsDir 'stickies_setup_9.0c.exe'
-
 $packageArgs = @{
-  packageName    = 'stickies'
-  fileFullPath   = $fileFullPath
-  url            = 'http://www.zhornsoftware.co.uk/stickies/stickies_setup_9.0c.exe'
-  checksum       = 'B0862703109A1B70E8A1E88953AB1C868F89E46775BF2D0DB084A2D9EC267364'
-  checksumType   = 'sha256'
+  packageName   = 'stickies'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = 'http://www.zhornsoftware.co.uk/stickies/stickies_setup_9.0c.exe'
+  silentArgs    = '-silent'
+  validExitCodes= @(0)
+  softwareName  = 'stickies*'
+  checksum      = 'B0862703109A1B70E8A1E88953AB1C868F89E46775BF2D0DB084A2D9EC267364'
+  checksumType  = 'sha256'
 }
 
-Get-ChocolateyWebFile @packageArgs
-
-# AutoIt
-$installerFile = Join-Path $toolsDir 'stickies.au3'
-write-host "Installing `'$fileFullPath`' with AutoIt3 using `'$installerFile`'"
-$installArgs = "/c autoit3 `"$installerFile`" `"$fileFullPath`""
-Start-ChocolateyProcessAsAdmin -Statements "$installArgs" -ExeToRun 'cmd.exe' -ValidExitCodes @(0)
+Install-ChocolateyPackage @packageArgs
