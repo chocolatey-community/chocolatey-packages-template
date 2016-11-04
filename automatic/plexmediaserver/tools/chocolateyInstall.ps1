a7a5b3f8ef968a16fdf7779fbb66bb87258782cc
mikecole <cole.mike@gmail.com>
2016-11-04 02:19:17 +0000

    AU:  updated - plexmediaserver [skip ci]

--- automatic/plexmediaserver/tools/chocolateyInstall.ps1
+++ automatic/plexmediaserver/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿$checksum = '255d9fe553d074d2d726f0caada49d42121fbd44bf86b792e65a4b17e66a4213'
+$checksum = '255d9fe553d074d2d726f0caada49d42121fbd44bf86b792e65a4b17e66a4213'
 $url = 'https://downloads.plex.tv/plex-media-server/1.2.5.2966-3f767e7/Plex-Media-Server-1.2.5.2966-3f767e7.exe'
 
 $packageArgs = @{
