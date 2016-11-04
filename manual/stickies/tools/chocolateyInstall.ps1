f5e67e0ab545d2706ae5173ff4487ec3ca1aa1ab
Mike Cole <cole.mike@gmail.com>
2016-11-03 22:50:33 -0500

    Upgraded stickies.

--- manual/stickies/tools/chocolateyInstall.ps1
+++ manual/stickies/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileFullPath = Join-Path $toolsDir 'stickies_setup_9.0b.exe'
+$fileFullPath = Join-Path $toolsDir 'stickies_setup_9.0c.exe'
 
 $packageArgs = @{
   packageName    = 'stickies'
   fileFullPath   = $fileFullPath
-  url            = 'http://www.zhornsoftware.co.uk/stickies/stickies_setup_9.0b.exe'
-  checksum       = '34E22C85A4DF4A2CFEF743589212B423CA60D84F1C14AA9AFB4682ADF5A2CA31'
+  url            = 'http://www.zhornsoftware.co.uk/stickies/stickies_setup_9.0c.exe'
+  checksum       = 'B0862703109A1B70E8A1E88953AB1C868F89E46775BF2D0DB084A2D9EC267364'
   checksumType   = 'sha256'
 }
 
