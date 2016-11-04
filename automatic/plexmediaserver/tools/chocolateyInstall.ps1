78332a1a97057a200531fbcf8b23b5909bcfcf57
Mike Cole <cole.mike@gmail.com>
2016-11-03 21:01:28 -0500

    Moved plexmediaserver to AU.

---
+++ automatic/plexmediaserver/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$checksum = '255d9fe553d074d2d726f0caada49d42121fbd44bf86b792e65a4b17e66a4213'
+$url = 'https://downloads.plex.tv/plex-media-server/1.2.5.2966-3f767e7/Plex-Media-Server-1.2.5.2966-3f767e7.exe'
+
+$packageArgs = @{
+  packageName   = 'plexmediaserver'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = $url
+  silentArgs    = '/quiet'
+  validExitCodes= @(0)
+  softwareName  = 'plexmediaserver*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/plexmediaserver/update.ps1
@@ -0,0 +1,31 @@
+import-module au
+
+$feed_url = 'https://plex.tv/downloads/details/1?build=windows-i386&distro=english'
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+			"(^[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    Write-Host $feed_url
+
+	[xml]$homepage_content = Invoke-WebRequest -UseBasicParsing -Uri $feed_url
+
+	$version = $homepage_content.DocumentElement.SelectSingleNode("Release").Attributes["version"].Value.Split("-")[0]
+
+	Write-Host $version
+
+	$url = $homepage_content.DocumentElement.SelectSingleNode("Release").SelectSingleNode("Package").Attributes["url"].Value
+
+	Write-Host $url
+
+    $Latest = @{ URL = $url; Version = $version }
+    return $Latest
+}
+
+update -NoCheckUrl -ChecksumFor 32
