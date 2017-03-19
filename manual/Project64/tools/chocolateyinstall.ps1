c0dc99fa838b7c2b9d0183cbed614f7e7068dca3
Scott Walters <me@scowalt.com>
2017-03-19 16:39:36 -0700

    Project64 v2.3.2 (#1)

---
+++ manual/Project64/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'Project64'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.pj64-emu.com/downloads/func-download/125'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'Project64*'
+
+  checksum      = 'FE8397EB045B92C450ABED075D98CC90CF3137461E410A65407AE8D2152BDCFD'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
