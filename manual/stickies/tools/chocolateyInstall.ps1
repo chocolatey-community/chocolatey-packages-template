d16211170667eb0e0aa993e50a07dfe30045cc1c
Mike Cole <cole.mike@gmail.com>
2017-04-04 20:57:03 -0500

    Updated stickies.

--- manual/stickies/tools/chocolateyInstall.ps1
+++ manual/stickies/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@ $packageArgs = @{
   packageName   = 'stickies'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'exe'
-  url           = 'http://www.zhornsoftware.co.uk/stickies/stickies_setup_9.0c.exe'
+  url           = 'http://www.zhornsoftware.co.uk/stickies/stickies_setup_9.0d.exe'
   silentArgs    = '-silent'
   validExitCodes= @(0)
   softwareName  = 'stickies*'
-  checksum      = 'B0862703109A1B70E8A1E88953AB1C868F89E46775BF2D0DB084A2D9EC267364'
+  checksum      = '5BC755FEDFA8FBED9F4F04F657CDAD2738253924B613C6D121BD4EE4F0723805'
   checksumType  = 'sha256'
 }
 
