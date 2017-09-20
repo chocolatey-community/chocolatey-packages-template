2560f9cbdee72bceaee6a5a6ba419cc0613632c0
mikecole <cole.mike@gmail.com>
2017-09-20 16:04:22 +0000

    AU: 1 updated - plexmediaserver [skip ci] https://gist.github.com/fc383d2a7d15fa7825ca98990d04357a/8f9117e09f80326a02e32e686054ae18c5922520

--- automatic/plexmediaserver/tools/chocolateyInstall.ps1
+++ automatic/plexmediaserver/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$checksum = 'b6d519ff298e4846ba3f3ba5a485c7e1759333f3deff33024c25897fa4592cd3'
-$url = 'https://downloads.plex.tv/plex-media-server/1.8.4.4249-3497d6779/Plex-Media-Server-1.8.4.4249-3497d6779.exe'
+﻿$checksum = '8b9bcb2f51944a4f320ae987fa204a8b21ea7024004577658fb38ce6df5c4a74'
+$url = 'https://downloads.plex.tv/plex-media-server/1.9.1.4272-b207937f1/Plex-Media-Server-1.9.1.4272-b207937f1.exe'
 
 $packageArgs = @{
   packageName   = 'plexmediaserver'
