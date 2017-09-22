b42abc403bf8fad53955f0fd1f2385b2953e6173
bcurran3 <bcurran3@yahoo.com>
2017-09-22 10:06:58 -0700

    Start PMS after update
    This is untested.
    -----------------
    This should test to see if PMS is running before an update and run it again after the update if so. It should not run PMS if it wasn't previously installed and running.
    -----------------
    I'll test when I have a chance.

--- automatic/plexmediaserver/tools/chocolateyInstall.ps1
+++ automatic/plexmediaserver/tools/chocolateyInstall.ps1
@@ -1,6 +1,12 @@
 ﻿$checksum = '8b9bcb2f51944a4f320ae987fa204a8b21ea7024004577658fb38ce6df5c4a74'
 $url = 'https://downloads.plex.tv/plex-media-server/1.9.1.4272-b207937f1/Plex-Media-Server-1.9.1.4272-b207937f1.exe'
 
+if ((get-process "Plex Media Server" -ea SilentlyContinue) -eq $Null){ 
+   $PMSRunning="False"
+  } else { 
+   $PMSRunning="True"
+ }
+ 
 $packageArgs = @{
   packageName   = 'plexmediaserver'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
@@ -14,3 +20,12 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs
+
+if ($PMSRunning="True"){
+    $bits = Get-ProcessorBits
+    if ($bits -eq 64){
+	   "$env:PROGRAMFILES\Plex\Plex Media Server\Plex Media Server.exe"
+     } else {
+      "$env:PROGRAMFILES(x86)\Plex\Plex Media Server\Plex Media Server.exe"
+     }
+	}
