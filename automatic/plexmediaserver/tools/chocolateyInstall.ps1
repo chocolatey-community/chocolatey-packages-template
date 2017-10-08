07658c856952dfb7019e7575ad0e1ad019e1bf1c
bcurran3 <bcurran3@yahoo.com>
2017-10-08 03:39:33 -0700

    Tests for and re-runs PMS if previously running
    Tested and working (x64).
    Upgrades cause PMS to shutdown.
    This checks to see if PMS is running or not before installing/upgrading. If PMS is found running, it re-runs PMS after the upgrade. Does not run, just as default, on a new install.
    No more finding my PMS not running due to an upgrade and having to pin it to avoid this problem and do manual upgrades. Yay!

--- automatic/plexmediaserver/tools/chocolateyInstall.ps1
+++ automatic/plexmediaserver/tools/chocolateyInstall.ps1
@@ -1,12 +1,13 @@
-﻿$checksum = '8b9bcb2f51944a4f320ae987fa204a8b21ea7024004577658fb38ce6df5c4a74'
+$checksum = '8b9bcb2f51944a4f320ae987fa204a8b21ea7024004577658fb38ce6df5c4a74'
 $url = 'https://downloads.plex.tv/plex-media-server/1.9.1.4272-b207937f1/Plex-Media-Server-1.9.1.4272-b207937f1.exe'
 
 if ((get-process "Plex Media Server" -ea SilentlyContinue) -eq $Null){ 
-   $PMSRunning="False"
-  } else { 
-   $PMSRunning="True"
- }
- 
+    $PMSRunning="False" # Always false on new install.
+   } else { 
+    $PMSRunning="True"
+    Write-Host "Plex Media Server found running." -foreground magenta 
+}
+
 $packageArgs = @{
   packageName   = 'plexmediaserver'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
@@ -21,11 +22,12 @@ $packageArgs = @{
 
 Install-ChocolateyPackage @packageArgs
 
-if ($PMSRunning="True"){
+if ($PMSRunning -eq "True"){
     $bits = Get-ProcessorBits
+	Write-Host "Running Plex Media Server" -foreground magenta 
     if ($bits -eq 64){
-	   "$env:PROGRAMFILES\Plex\Plex Media Server\Plex Media Server.exe"
-     } else {
-      "$env:PROGRAMFILES(x86)\Plex\Plex Media Server\Plex Media Server.exe"
-     }
-	}
+	& "${env:PROGRAMFILES(x86)}\Plex\Plex Media Server\Plex Media Server.exe"
+       } else {
+        & "$env:PROGRAMFILES\Plex\Plex Media Server\Plex Media Server.exe"
+       }
+}
