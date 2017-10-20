ceb9b404b0d578c8207a8b052cd57c8980fee115
Mike Cole <cole.mike@gmail.com>
2017-10-20 15:04:39 -0500

    Added baseline ssdt17 package.

---
+++ automatic/ssdt17/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+$checksum = 'b9cad935be8613aa02c5251d32b05433a2dadf2284d03eb4afa8ca4f81e9036c'
+$url = 'https://go.microsoft.com/fwlink/?LinkID=832313'
+
+(Get-WmiObject -Class Win32_OperatingSystem).Version -match "(?<Major>\d+).(?<Minor>\d+).(?<Build>\d+)" | Out-Null
+ 
+if ($Matches.Major -eq 6 -and $Matches.Minor -eq 3)
+{
+    # Windows 8.1 / Server 2012 R2 requires a prerequisite hotfix 
+    if (-not (Get-HotFix -Id KB2919355 -ErrorAction SilentlyContinue))
+    {
+        Write-Error "A prerequisite for installing SQL Server Data Tools for Visual Studio 2017 on Windows 8.1 and Windows Server 2012 R2 is to have hotfix KB2919355 installed. See https://msdn.microsoft.com/library/ms143506.aspx for more details"
+    }
+}
+
+$packageArgs = @{
+  packageName   = 'ssdt17'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = $url
+  silentArgs    = '/passive /norestart'
+  validExitCodes= @(0)
+  softwareName  = 'ssdt17*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/ssdt17/update.ps1
@@ -0,0 +1,33 @@
+import-module au
+
+$notes_url = 'https://docs.microsoft.com/en-us/sql/ssdt/download-sql-server-data-tools-ssdt'
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+			"(^[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    Write-Host $notes_url
+
+    $notes_content = Invoke-WebRequest -Uri $notes_url
+
+    $link = $notes_content.ParsedHtml.getElementsByTagName("a") | where {$_.innerText -like "*Download SSDT for Visual Studio 2015*"}
+    Write-Host $link.href
+
+	$temp_file = $env:TEMP + '\SSDTSetup.exe'
+	Invoke-WebRequest $link.href -OutFile $temp_file
+	Write-Host $temp_file
+
+	$version = (Get-Command $temp_file).Version
+	Write-Host $version
+
+    $Latest = @{ URL = $link.href; Version = $version }
+    return $Latest
+}
+
+update -NoCheckUrl -ChecksumFor 32
