d542a9234875e84d920a4dc13b52d9f4d5ced419
Mike Cole <cole.mike@gmail.com>
2018-01-06 14:27:09 -0600

    Moved MASE to manual.

--- manual/microsoftazurestorageexplorer/tools/chocolateyInstall.ps1
+++ manual/microsoftazurestorageexplorer/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-﻿$checksum = '15615F7F5561984D89573077D41005AC55B951C777A5336DDCE8DEFEF5B28401'
+﻿$checksum = '1AA0726FE1E4CFBAD23572F4CCBF60B6A87531908DC159C19628A263FAEFE8BB'
 
 $packageArgs = @{
   packageName   = 'microsoftazurestorageexplorer'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'exe'
-  url           = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
+  url           = 'http://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
   silentArgs    = '/VERYSILENT'
   validExitCodes= @(0)
   softwareName  = 'microsoftazurestorageexplorer*'
