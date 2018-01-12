42b2f476cc5d82be9af6444d8acfee22ef601218
asif.shaikh <ripclaw_ffb@hotmail.com>
2018-01-11 20:13:25 -0800

    setup appveyor.yml and add telegraf package

---
+++ automatic/telegraf/tools/chocolateyinstall.ps1
@@ -0,0 +1,42 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$unzip_folder    = $env:ProgramFiles
+$install_folder  = "$unzip_folder\telegraf"
+$configDirectory = Join-Path $install_folder 'telegraf.d'
+$packageName     = 'telegraf'
+$softwareName    = 'telegraf*'
+$toolsDir        = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url             = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.5.1_windows_i386.zip'
+$url64           = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.5.1_windows_amd64.zip'
+$fileLocation    = Join-Path $install_folder 'telegraf.exe'
+
+If(!(Test-Path -Path $configDirectory)){
+  New-Item -Path $configDirectory -ItemType Directory
+}
+
+If (Get-Service -Name "telegraf" -ErrorAction SilentlyContinue) {
+    & $fileLocation --service uninstall
+}
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $unzip_folder
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  file          = $fileLocation
+  file64        = $fileLocation
+
+  softwareName  = 'telegraf*'
+
+  checksum       = 'fdc8f33f3bffe445cc5e9d5ea9b45f9fbbc5068b4090b152d3b69b5f0cf5cfa2'
+  checksumType   = 'sha256'
+  checksum64     = 'afba1bf33f221a6e16a1c48e996abfd02e00037185ddf9082869108dbf4c5351'
+  checksumType64 = 'sha256'
+
+  silentArgs     = "--config-directory `"$configDirectory`" --service install"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
---
+++ automatic/telegraf/tools/chocolateyuninstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'telegraf'
+$softwareName = 'telegraf*'
+$installerType = 'EXE'
+$install_folder = Join-Path $env:ProgramFiles 'telegraf'
+
+$silentArgs = '--service uninstall'
+$validExitCodes = @(0)
+
+$uninstalled = $false
+
+$file = Join-Path $install_folder 'telegraf.exe'
+
+Uninstall-ChocolateyPackage -PackageName $packageName `
+                            -FileType $installerType `
+                            -SilentArgs "$silentArgs" `
+                            -ValidExitCodes $validExitCodes `
+                            -File "$file"
+
+Start-Sleep -Seconds 3
+Remove-Item -Path $install_folder -Recurse -Confirm:$false -Force
