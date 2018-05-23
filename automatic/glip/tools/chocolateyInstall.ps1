872c96babbe142c36cb4aef99e553094498ad28f
Mike Cole <cole.mike@gmail.com>
2018-05-23 00:22:05 -0500

    Moved glip to AU

--- automatic/glip/tools/chocolateyInstall.ps1
+++ automatic/glip/tools/chocolateyInstall.ps1
@@ -1,12 +1,14 @@
+$checksum = '8F00B17D20F8B02079581CF9E0A757A13124B94210FC757DC7B8254789E681E8'
+
 $packageArgs = @{
   packageName   = 'glip'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'exe'
-  url           = 'https://downloads.ringcentral.com/glip/rc/18.01.1/x64/Glip-18.01.1-x64.exe'
+  url           = 'http://downloads.ringcentral.com/glip/rc/GlipForWindows64'
   silentArgs    = '/VERYSILENT'
   validExitCodes= @(0)
   softwareName  = 'glip*'
-  checksum      = '8F00B17D20F8B02079581CF9E0A757A13124B94210FC757DC7B8254789E681E8'
+  checksum      = $checksum
   checksumType  = 'sha256'
 }
 
