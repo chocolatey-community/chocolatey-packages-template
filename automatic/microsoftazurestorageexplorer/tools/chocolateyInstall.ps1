1f4c940a0c8215086912e2864e92b14b5a271772
Mike Cole <cole.mike@gmail.com>
2018-05-23 10:29:46 -0500

    Re-added MASE to AU

---
+++ automatic/microsoftazurestorageexplorer/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$checksum = '15615F7F5561984D89573077D41005AC55B951C777A5336DDCE8DEFEF5B28401'
+
+$packageArgs = @{
+  packageName   = 'microsoftazurestorageexplorer'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0)
+  softwareName  = 'microsoftazurestorageexplorer*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
