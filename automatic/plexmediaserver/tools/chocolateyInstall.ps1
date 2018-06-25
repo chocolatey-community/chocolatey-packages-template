588b878db910009ab72d10d3f9a4d78db5e799b8
Mike Cole <cole.mike@gmail.com>
2018-06-24 22:36:51 -0500

    Added crystalreports-for-vs

--- automatic/plexmediaserver/tools/chocolateyInstall.ps1
+++ automatic/plexmediaserver/tools/chocolateyInstall.ps1
@@ -1,12 +1,7 @@
 ﻿$checksum = '8a11f4dedee9adc17ee90a459b38141fa4a90b3843fe700c04b155ccbf74c110'
 $url = 'https://downloads.plex.tv/plex-media-server/1.13.2.5154-fd05be322/Plex-Media-Server-1.13.2.5154-fd05be322.exe'
 
-if ((get-process "Plex Media Server" -ea SilentlyContinue) -eq $Null){ 
-    $PMSRunning="False" # Always false on new install.
-   } else { 
-    $PMSRunning="True"
-    Write-Host "Plex Media Server found running." -foreground magenta 
-}
+Start-CheckandStop "Plex Media Server"
 
 $packageArgs = @{
   packageName   = 'plexmediaserver'
@@ -22,12 +17,4 @@ $packageArgs = @{
 
 Install-ChocolateyPackage @packageArgs
 
-if ($PMSRunning -eq "True"){
-    $bits = Get-ProcessorBits
-	Write-Host "Running Plex Media Server" -foreground magenta 
-    if ($bits -eq 64){
-	& "${env:PROGRAMFILES(x86)}\Plex\Plex Media Server\Plex Media Server.exe"
-       } else {
-        & "$env:PROGRAMFILES\Plex\Plex Media Server\Plex Media Server.exe"
-       }
-}
+if ($ProcessWasRunning -eq "True") {&$ProcessFullPath}
---
+++ manual/crystalreports-for-visualstudio/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$packageName = 'crystalreports-for-visualstudio'
+$installerType = 'MSI'
+$url = 'https://downloads.businessobjects.com/akdlm/cr4vs2010/CRforVS_13_0_22.exe'
+$checksum = 'E05CDA365DD11DE1E279DDF58113F7612B4E5DA6970D228F32E415296D2F2F2E'
+$silentArgsSDK = "/q /l ""$env:temp\$packageName-SDK.log"" UPGRADE=1"
+$silentArgsRuntime = "/q /l ""$env:temp\$packageName-Runtime.log"" UPGRADE=1"
+
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Get-ChocolateyWebFile 'crystalreports-for-visualstudio' "$toolsDir\CRforVS_13_0_22.exe" "$url" -Checksum "$checksum" -ChecksumType 'sha256'
+Get-ChocolateyUnzip "$toolsDir\CRforVS_13_0_22.exe" "$toolsDir\CRforVS_13_0_22"
+Remove-Item "$toolsDir\CRforVS_13_0_22.exe"
+
+$sdkFileFullPath = get-childitem $toolsDir -recurse -include CrystalReportsForVisualStudio.msi | select -First 1
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgsSDK" "$sdkFileFullPath"
+
+$is64bit = Get-ProcessorBits 64;
+if ($is64bit) {
+	$runtimeFileFullPath = get-childitem $toolsDir -recurse -include CRRuntime_64bit_13_0_22.msi | select -First 1
+	Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgsRuntime" "$runtimeFileFullPath"
+}
+
+Remove-Item "$toolsDir\CRforVS_13_0_22" -recurse
