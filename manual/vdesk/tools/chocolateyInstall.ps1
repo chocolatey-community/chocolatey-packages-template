6406f4bfc115bb311090aa7e94702d834f4d1ac7
Mike Cole <cole.mike@gmail.com>
2018-12-04 21:01:45 -0600

    Updated vdesk

--- manual/vdesk/tools/chocolateyInstall.ps1
+++ manual/vdesk/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@ $packageArgs = @{
   packageName   = 'vdesk'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'msi'
-  url           = 'https://github.com/eksime/VDesk/releases/download/v1.1.2/VDeskSetup.msi'
+  url           = 'https://github.com/eksime/VDesk/releases/download/v1.2.0/VDeskSetup.msi'
   silentArgs    = '/qn /norestart'
   validExitCodes= @(0)
   softwareName  = 'VDesk*'
-  checksum      = '3870FBF7217D0FBD5B7B66B4C6C017926AB3CB6B93247CF665255B52661B09A0'
+  checksum      = 'F3971A8845F3BAABF2D988187F17FFE21B0397B17E4A68FB1AD92BC6010FDE6F'
   checksumType  = 'sha256'
 }
 
