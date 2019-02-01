007835cf5cc4efb988ff03b8f629934e2ddfc315
Mike Cole <cole.mike@gmail.com>
2019-01-31 19:54:45 -0600

    Updated azuredatafactorytools15.

--- manual/azuredatafactorytools15/tools/chocolateyInstall.ps1
+++ manual/azuredatafactorytools15/tools/chocolateyInstall.ps1
@@ -1,3 +1,5 @@
+"$ErrorActionPreference = 'Stop'"
+
 $packageArgs = @{
   packageName   = 'azuredatafactorytools15'
   vsixUrl       = 'https://visualstudiogallery.msdn.microsoft.com/371a4cf9-0093-40fa-b7dd-be3c74f49005/file/182173/6/AzureDataFactoryVisualStudioTools-VS2015.vsix'
