b92f85f59130c5d04f09f7b8c2691c708541a2e3
Mike Cole <cole.mike@gmail.com>
2019-02-01 03:03:47 -0600

    Created ms-data-migration-assistant

---
+++ manual/ms-data-migration-assistant/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = 'ms-data-migration-assistant'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'msi'
+  url           = 'https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi'
+  silentArgs    = '/quiet /norestart'
+  validExitCodes= @(0)
+  softwareName  = 'Microsoft Data Migration Assistant*'
+  checksum      = 'CC88BB2FB08C7AE020897DDE33F49E0A0C2EAC9C2FA8DF4E8A8AADDE88E46A79'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
