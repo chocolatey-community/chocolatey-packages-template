c5b02b31d653e34eddc3961400039d11f9a8f54b
Mike Cole <cole.mike@gmail.com>
2019-02-05 10:25:30 -0600

    Adjusted azuredatafactorytools

--- manual/azuredatafactorytools15/tools/chocolateyInstall.ps1
+++ manual/azuredatafactorytools15/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
   packageName   = 'azuredatafactorytools15'
-  vsixUrl       = 'https://visualstudiogallery.msdn.microsoft.com/371a4cf9-0093-40fa-b7dd-be3c74f49005/file/182173/6/AzureDataFactoryVisualStudioTools-VS2015.vsix'
+  vsixUrl       = 'https://azuredatafactory.gallerycdn.vsassets.io/extensions/azuredatafactory/microsoftazuredatafactorytoolsforvisualstudio2015/0.9.3527.2/1482142461622/182173/9/AzureDataFactoryVisualStudioTools-VS2015.vsix'
   vsVersion     =  14
   checksum      = '095C4ECD7BC1796D497920385BD198B4231388616F9458A919F593CC84D74B89'
   checksumType  = 'sha256'
