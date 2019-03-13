ac75f905213c833627864f621d72a311463a0928
Mike Cole <cole.mike@gmail.com>
2019-03-12 19:37:36 -0500

    Added uninstall script to juffed.

---
+++ manual/juffed/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$installFolder = "$(Get-ToolsLocation)\JuffEd_0.8.1"
+
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+
+$installShortcut = "$desktop\JuffEd.lnk";
+
+if (Test-Path $installFolder) {
+  Remove-Item -Force -Recurse -ea 0 $installFolder
+}
+
+if (Test-Path $installShortcut) {
+  Remove-Item -Force -ea 0 $installShortcut
+}
