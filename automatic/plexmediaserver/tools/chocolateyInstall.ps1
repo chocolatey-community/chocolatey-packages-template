85338b52ff42bd2920c086d6e83cd00174e58f98
mikecole <cole.mike@gmail.com>
2019-05-24 21:07:26 +0000

    AU: 1 updated - plexmediaserver [skip ci] https://gist.github.com/fc383d2a7d15fa7825ca98990d04357a/f8c187bce8cb52c5619556a43cd7729504935eb6

--- automatic/plexmediaserver/tools/chocolateyInstall.ps1
+++ automatic/plexmediaserver/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$checksum = '415496f54b873623a6d8ff2c692d48c1bfeb8dd15578ec45b94a24c95ffa3c10'
-$url = 'https://downloads.plex.tv/plex-media-server-new/1.15.4.994-107756f7e/windows/PlexMediaServer-1.15.4.994-107756f7e-x86.exe'
+﻿$checksum = '096462d8c38855bf7afd2e410cbecfa686a19f7c11fb9d6402b95590721dd7a6'
+$url = 'https://downloads.plex.tv/plex-media-server-new/1.15.6.1079-78232c603/windows/PlexMediaServer-1.15.6.1079-78232c603-x86.exe'
 
 Start-CheckandStop "Plex Media Server"
 
