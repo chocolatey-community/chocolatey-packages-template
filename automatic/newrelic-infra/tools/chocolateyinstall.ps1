ccd777444d63d8947d21edba4fa1009c8f66d322
Chocolatey <chocolatey@realdimensions.net>
2019-10-08 00:05:38 +0000

    AU: 2 updated - newrelic-infra telegraf [skip ci] https://gist.github.com/cbab2a407ba1e3def260bb8a9d3ec454/6cd8817175a2597295d0d3067ba3887626183960

--- automatic/newrelic-infra/tools/chocolateyinstall.ps1
+++ automatic/newrelic-infra/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageName     = 'newrelic-infra'
 $softwareName    = 'newrelic-infra*'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://download.newrelic.com/infrastructure_agent/windows/newrelic-infra.1.5.45.msi'
+$url64      = 'https://download.newrelic.com/infrastructure_agent/windows/newrelic-infra.1.5.51.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,7 +14,7 @@ $packageArgs = @{
 
   softwareName  = $softwareName
 
-  checksum64     = 'd99a39b6f941a1f65d199a1fef322c7dc4cde98f3efe0b402042531edc9e1462'
+  checksum64     = '7b5d21d4797978c548a7f8d87d64838de2a0eb71893d52578dbb7cf081bf0c3d'
   checksumType64 = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/telegraf/tools/chocolateyinstall.ps1
+++ automatic/telegraf/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $configDirectory = Join-Path $install_folder 'telegraf.d'
 $packageName     = 'telegraf'
 $softwareName    = 'telegraf*'
 $toolsDir        = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url             = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.12.2_windows_i386.zip'
-$url64           = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.12.2_windows_amd64.zip'
+$url             = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.12.3_windows_i386.zip'
+$url64           = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.12.3_windows_amd64.zip'
 $fileLocation    = Join-Path $install_folder 'telegraf.exe'
 
 If(!(Test-Path -Path $configDirectory)){
@@ -29,9 +29,9 @@ $packageArgs = @{
 
   softwareName  = 'telegraf*'
 
-  checksum       = '4a613accf89044f98198b9fc303367df083e6e7c0bf1d26d37e74ef746f755ad'
+  checksum       = '7d9ba335f68bc9ef301370c42d08399ed5e31630f8e28598da31cf141c29441f'
   checksumType   = 'sha256'
-  checksum64     = '21079f1171caf7f39f216e7582ff4196be6afefc29a0874a11dc55912b04ec56'
+  checksum64     = '1d5fad004f37aa830806191f8dc78e07450e3c710388bc6229e6dc29c17c3245'
   checksumType64 = 'sha256'
 
   silentArgs     = "--config-directory `"$configDirectory`" --service install"
