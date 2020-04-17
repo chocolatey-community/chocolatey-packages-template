b8d7f74a6628ee86e97684e2ce3bb528ee719a8d
Scott Walters <me@scowalt.com>
2020-04-17 13:08:11 -0700

    cryptomator version 1.5.0

--- manual/cryptomator/tools/chocolateyInstall.ps1
+++ manual/cryptomator/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'cryptomator'
   fileType               = 'exe'
-  url64bit               = 'https://dl.bintray.com/cryptomator/cryptomator/1.4.15/Cryptomator-1.4.15-x64.exe'
-  checksum64             = 'E7618F0EBA90012370D9EBC98E01496D5069728441E40D312AAAB9316DC6A740'
+  url64bit               = 'https://dl.bintray.com/cryptomator/cryptomator/1.5.0/Cryptomator-1.5.0-x64.exe'
+  checksum64             = '3295CF314BC67BB866AB0830478106CF55D203A5953A9CB304186DFE1DFB81F9'
   checksumType64         = 'sha256'
   silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes         = @(0)
