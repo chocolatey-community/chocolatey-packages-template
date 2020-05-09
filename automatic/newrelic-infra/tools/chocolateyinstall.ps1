9187f4b667c1ebd3cbb3d46798edf439ceb0f1c6
ripclawffb <ripclaw_ffb@hotmail.com>
2020-05-08 21:44:03 -0700

    Add support for newrelic-infra install parameters

--- automatic/newrelic-infra/tools/chocolateyinstall.ps1
+++ automatic/newrelic-infra/tools/chocolateyinstall.ps1
@@ -1,5 +1,36 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$additionalArgs = ''
+$packageParameters = Get-PackageParameters
+
+# see if any parameters were passed
+if ($packageParameters['GenerateConfig']) { $GenerateConfig = $packageParameters['GenerateConfig'] }
+if ($packageParameters['LicenseKey']) { $LicenseKey = $packageParameters['LicenseKey'] }
+if ($packageParameters['DisplayName']) { $DisplayName = $packageParameters['DisplayName'] }
+if ($packageParameters['Proxy']) { $Proxy = $packageParameters['Proxy'] }
+if ($packageParameters['CustomAttributes']) { $CustomAttributes = $packageParameters['CustomAttributes'] }
+
+if(Get-Variable -Name GenerateConfig -ErrorAction SilentlyContinue) {
+  $additionalArgs = 'GENERATE_CONFIG=true'
+
+  if(Get-Variable -Name LicenseKey -ErrorAction SilentlyContinue) {
+    $additionalArgs += " LICENSE_KEY=$LicenseKey"
+  }
+
+  if(Get-Variable -Name DisplayName -ErrorAction SilentlyContinue) {
+    $additionalArgs += " DISPLAY_NAME=$DisplayName"
+  }
+
+  if(Get-Variable -Name Proxy -ErrorAction SilentlyContinue) {
+    $additionalArgs += " PROXY=$Proxy"
+  }
+
+  if(Get-Variable -Name CustomAttributes -ErrorAction SilentlyContinue) {
+    $additionalArgs += " CUSTOM_ATTRIBUTES=$CustomAttributes"
+  }
+}
+
+
 $packageName     = 'newrelic-infra'
 $softwareName    = 'newrelic-infra*'
 
@@ -17,27 +48,8 @@ $packageArgs = @{
   checksum64     = '25f48365e88f166e2451c07ee14e21c69e8ec656283b988abbd1c2ec6e1f236f'
   checksumType64 = 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs    = "/qn $additionalArgs /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
