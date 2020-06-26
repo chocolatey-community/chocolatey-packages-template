5fd433bc53787afeb75cba4fb722b387f0d8d408
Sam <fertilefields@gmx.ch>
2020-06-26 19:07:20 +0200

    Revert "Fix URL: Don't use https"
    This reverts commit 5365ad2d8e39f795aee87b72c0109a6d5cc1c78a.

--- automatic/rtl-utility/tools/chocolateyinstall.ps1
+++ automatic/rtl-utility/tools/chocolateyinstall.ps1
@@ -25,10 +25,10 @@ else
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName  
   fileFullPath   = [System.IO.Path]::Combine($installDir, $exeName)
-  url            = 'http://oblique-audio.com/downloads/' + $exeName32
+  url            = 'https://oblique-audio.com/downloads/' + $exeName32
   checksum       = '5D7F341B8B84F23A5AC547210878489B7E70BC1BA820A78A4C4BD2F2B80A11DE'
   checksumType   = 'sha256'
-  url64          = 'http://oblique-audio.com/downloads/' + $exeName64
+  url64          = 'https://oblique-audio.com/downloads/' + $exeName64
   checksum64     = 'AE3BDE45FDB803E36D91A5604366AD4FD7C91DF4B492958D3B4FE79481407091'
   checksumType64 = 'sha256'
 }
