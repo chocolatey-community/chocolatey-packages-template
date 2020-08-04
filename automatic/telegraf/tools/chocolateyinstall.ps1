c8a6a19d8594c80fac0fa3b0f7da3d25341644eb
Asif Shaikh <ripclaw_ffb@hotmail.com>
2020-08-03 21:36:03 -0700

    Update telegraf package logic

--- automatic/telegraf/tools/chocolateyinstall.ps1
+++ automatic/telegraf/tools/chocolateyinstall.ps1
@@ -1,35 +1,42 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
-$unzip_folder    = $env:ProgramFiles
-$install_folder  = "$unzip_folder\telegraf"
-$configDirectory = Join-Path $install_folder 'telegraf.d'
+$unzipFolder    = $env:ProgramFiles
+$installFolder  = "$unzipFolder\telegraf"
+$configDirectory = Join-Path $installFolder 'telegraf.d'
 $packageName     = 'telegraf'
 $softwareName    = 'telegraf*'
 $toolsDir        = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url             = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.15.2_windows_i386.zip'
 $url64           = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.15.2_windows_amd64.zip'
-$fileLocation    = Join-Path $install_folder 'telegraf.exe'
+$fileLocation    = Join-Path $installFolder 'telegraf.exe'
 $telegrafRegPath = "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Application\telegraf"
+
+# Extract current version of telegraf
+$versionSearch = $url -match '.*-(\d*\.\d*\.\d*)_.*'
+if ($versionSearch) {
+  $version = $matches[1]
+}
 
 If(!(Test-Path -Path $configDirectory)){
   New-Item -Path $configDirectory -ItemType Directory
 }
 
 If (Get-Service -Name "telegraf" -ErrorAction SilentlyContinue) {
-    & $fileLocation --service uninstall
+  $servicePath = (Get-WmiObject win32_service | ?{$_.Name -like 'telegraf'}).PathName.Split('--')[0].Trim().Replace("""","")
+  & $servicePath --service uninstall
 }
 
 If (Test-Path $telegrafRegPath) {
-    Remove-Item $telegrafRegPath -Force
+  Remove-Item $telegrafRegPath -Force
 }
 
-If (Test-Path "$env:ProgramFiles\telegraf\telegraf.conf" -ErrorAction SilentlyContinue) {
-  Copy-Item -Force -Path "$env:ProgramFiles\telegraf\telegraf.conf" -Destination "$env:ProgramFiles\telegraf\telegraf.backup.conf"
+If (Test-Path "$installFolder\telegraf.conf" -ErrorAction SilentlyContinue) {
+  Copy-Item -Force -Path "$installFolder\telegraf.conf" -Destination "$installFolder\telegraf.backup.conf"
 }
 
 $packageArgs = @{
   packageName   = $packageName
-  unzipLocation = $unzip_folder
+  unzipLocation = $unzipFolder
   fileType      = 'EXE'
   url           = $url
   url64bit      = $url64
@@ -37,7 +44,7 @@ $packageArgs = @{
   file64        = $fileLocation
 
   softwareName  = 'telegraf*'
-
+  
   checksum       = 'a26e63df9ecf358c99696afe6de3cf343b3a31f44a254ee8a57bdbe81b9bcd73'
   checksumType   = 'sha256'
   checksum64     = 'b343a62d34c8604dc7b892de519a71968318466086c6ccf0b83237d1744cbe21'
@@ -48,9 +55,18 @@ $packageArgs = @{
 }
 
 Install-ChocolateyZipPackage @packageArgs
+
+# Move files to non versioned telegraf folder
+If((Test-Path -Path "$installFolder-$version")){
+  Write-Host "Moving telegraf files"
+  Move-Item -Force -Path "$installFolder-$version\*" -Destination "$installFolder\"
+  Remove-Item -Path "$installFolder-$version" -Recurse -Force
+}
+
 Install-ChocolateyInstallPackage @packageArgs
 
-If (Test-Path "$env:ProgramFiles\telegraf\telegraf.backup.conf" -ErrorAction SilentlyContinue) {
-  Move-Item -Force -Path "$env:ProgramFiles\telegraf\telegraf.backup.conf" -Destination "$env:ProgramFiles\telegraf\telegraf.conf"
+If (Test-Path "$installFolder\telegraf.backup.conf" -ErrorAction SilentlyContinue) {
+  Move-Item -Force -Path "$installFolder\telegraf.backup.conf" -Destination "$installFolder\telegraf.conf"
   Restart-Service -Name "telegraf"
 }
+
