f5e11850c656b827b2c31fe9ccb9bf20253cd5e0
Scott Walters <me@scowalt.com>
2020-08-15 07:48:04 -0700

    cryptomator 1.5.7

--- manual/cryptomator/tools/chocolateyInstall.ps1
+++ manual/cryptomator/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'cryptomator'
   fileType               = 'exe'
-  url64bit               = 'https://dl.bintray.com/cryptomator/cryptomator/1.5.6/Cryptomator-1.5.6-x64.exe'
-  checksum64             = '1EAAA64F81C11FA3C9FFA0A0DE68F53A1D21231733BBE243EF500DAEA3E88D69'
+  url64bit               = 'https://dl.bintray.com/cryptomator/cryptomator/1.5.7/Cryptomator-1.5.7-x64.exe'
+  checksum64             = '17F93203DFC822D921D2D53E9659A3F502A9CD0A175A8A5F6B387A8CEC9214EC'
   checksumType64         = 'sha256'
   silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes         = @(0)
@@ -17,4 +17,3 @@ if ($installLocation)  {
     Write-Host "$packageName installed to '$installLocation'"
 }
 else { Write-Warning "Can't find $PackageName install location" }
-
