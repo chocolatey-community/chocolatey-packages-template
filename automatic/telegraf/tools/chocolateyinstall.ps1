0deeeccb3e998bb2192d8c4e95faa246b402da0e
Chocolatey <chocolatey@realdimensions.net>
2020-12-03 00:01:54 +0000

    AU: 1 updated - telegraf [skip ci] https://gist.github.com/cbab2a407ba1e3def260bb8a9d3ec454/3efdb1d47e824c006f584546e970895d03c33078

--- automatic/telegraf/tools/chocolateyinstall.ps1
+++ automatic/telegraf/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $configDirectory = Join-Path $installFolder 'telegraf.d'
 $packageName     = 'telegraf'
 $softwareName    = 'telegraf*'
 $toolsDir        = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url             = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.16.2_windows_i386.zip'
-$url64           = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.16.2_windows_amd64.zip'
+$url             = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.16.3_windows_i386.zip'
+$url64           = 'https://dl.influxdata.com/telegraf/releases/telegraf-1.16.3_windows_amd64.zip'
 $fileLocation    = Join-Path $installFolder 'telegraf.exe'
 $telegrafRegPath = "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Application\telegraf"
 
@@ -45,9 +45,9 @@ $packageArgs = @{
 
   softwareName  = 'telegraf*'
   
-  checksum       = '12f8fdd2ea9fdbbda2f672692fd03bb68fe7bef690126228591b8285fa4c0fae'
+  checksum       = 'd802321d068e09cccd0f44d4704a5e9206612954a1b6c10a0a964bb31f6d30cb'
   checksumType   = 'sha256'
-  checksum64     = '6e1d856160bf9892b1c8421f407bab2d35a8567300c02efa64643c2a7640b25d'
+  checksum64     = '85fb7b0f96f8d3c42848a651d84beb34f0b7101864496248891834b36b2196cc'
   checksumType64 = 'sha256'
 
   silentArgs     = "--config-directory `"$configDirectory`" --service install"
