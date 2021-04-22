dfce5cafdcfc9caadfec228bd1e1f4a2ec14de24
Asif Shaikh <ripclaw_ffb@hotmail.com>
2021-04-21 22:19:35 -0700

    Upgrade terracreds package

--- automatic/terracreds/tools/chocolateyinstall.ps1
+++ automatic/terracreds/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/tonedefdev/terracreds/releases/download/v1.0.3/terracreds_1.0.3_windows_amd64.zip'
+$url        = 'https://github.com/tonedefdev/terracreds/releases/download/v1.1.0/terracreds_1.1.0_windows_amd64.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
 
-  checksum      = ''
+  checksum      = '7649bffd18fef3aa007143cf294cace8e0c89a42a0b74ccc79349a7c8d0f5da9'
   checksumType  = 'sha256'
 }
 
