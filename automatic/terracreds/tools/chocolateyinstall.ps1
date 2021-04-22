cb5a2e1853805a61c761e2bb8c05e0eaf49a429c
Asif Shaikh <ripclaw_ffb@hotmail.com>
2021-04-21 22:05:09 -0700

    Add Terracreds Package

---
+++ automatic/terracreds/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/tonedefdev/terracreds/releases/download/v1.0.3/terracreds_1.0.3_windows_amd64.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+
+  checksum      = '3a7dad16b3e68b725c2ce69645b8a932b1dd0cbd28f629efa8c4ba1e125c985e'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/terracreds/update.ps1
@@ -0,0 +1,42 @@
+import-module au
+
+$releases = 'https://github.com/tonedefdev/terracreds/releases/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^[$]packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+            "(?i)(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL)'"
+            "(checksum\s*=\s*)('.*')"             = "`$1'$($Latest.Checksum)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $match = $download_page.Content | Select-String -Pattern '(download/.*terracreds.*windows_amd64\.zip)'
+    $url_suffix = $match.Matches[0].value
+
+    $system_version = ($url_suffix -split '[_-]|[_-]windows_amd64\.|.zip') -match "^\d*\.\d*\.\d*$" | %{ new-object System.Version ($_) } | Sort-Object | Select-Object -Last 1
+    $version = "$($system_version.Major).$($system_version.Minor).$($system_version.Build)"
+    $url = "https://github.com/tonedefdev/terracreds/releases/download/v$($version)/terracreds_$($version)_windows_amd64.zip"
+
+
+    @{
+        Version      = $version
+        URL          = $url
+        ReleaseNotes = 'https://github.com/tonedefdev/terracreds/releases'
+    }
+}
+
+try {
+    update -ChecksumFor all
+} catch {
+    $ignore = 'Unable to connect to the remote server'
+    if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
+}
