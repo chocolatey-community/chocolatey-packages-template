6f7cfc1513b4a85fc0796695c6708ab4d418d4bb
Chocolatey <chocolatey@realdimensions.net>
2021-04-22 05:10:07 +0000

    AU: 1 updated - terracreds [skip ci] https://gist.github.com/cbab2a407ba1e3def260bb8a9d3ec454/6f90e2af59ec26b22c1fc02a4dea62554982787d

--- automatic/terracreds/tools/chocolateyinstall.ps1
+++ automatic/terracreds/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/tonedefdev/terracreds/releases/download/v1.0.3/terracreds_1.0.3_windows_amd64.zip'
+$url        = 'https://github.com/tonedefdev/terracreds/releases/download/v1.1.0/terracreds_1.1.0_windows_amd64.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
 
-  checksum      = '3a7dad16b3e68b725c2ce69645b8a932b1dd0cbd28f629efa8c4ba1e125c985e'
+  checksum      = ''
   checksumType  = 'sha256'
 }
 
