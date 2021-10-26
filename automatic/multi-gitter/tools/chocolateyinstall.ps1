0fdf1dd4346490a7c9d08b2f26703ea934155398
Sam <fertilefields@tutamail.com>
2021-10-26 20:22:52 +0200

    Fix extraction of .tar.gz file

--- automatic/multi-gitter/tools/chocolateyinstall.ps1
+++ automatic/multi-gitter/tools/chocolateyinstall.ps1
@@ -10,9 +10,13 @@ if ($pp.InstallDir)
 }
 Write-Host "$env:ChocolateyPackageName is going to be installed in '$installDir'"
 
+# Create temporary directory to extract .tar.gz -> .tar
+$tempDir = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), [System.Guid]::NewGuid())
+New-Item -ItemType Directory -Path $tempDir
+
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName  
-  unzipLocation  = $installDir
+  unzipLocation  = $tempDir
   url            = 'https://github.com/lindell/multi-gitter/releases/download/v0.36.0/multi-gitter_0.36.0_Windows_i386.tar.gz'
   checksum       = 'CEAED3CAA8729ECB8ABD0BD487A21FCA078DEFA5254A1F8549A54E793EBCAD64'
   checksumType   = 'sha256'
@@ -21,4 +25,10 @@ $packageArgs = @{
   checksumType64 = 'sha256'  
 }
 
+# Extract .tar.gz into temporary directory
 Install-ChocolateyZipPackage @packageArgs
+# Extract .tar into installation directory
+$File = Get-ChildItem -File -Path $tempDir -Filter *.tar
+Get-ChocolateyUnzip -fileFullPath $File.FullName -destination $installDir
+# Remove temporary directory
+Remove-Item $tempDir -Recurse
