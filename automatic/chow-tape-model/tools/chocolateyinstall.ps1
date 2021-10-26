4fbac46275c8af7d5cecdf61b897e0f6149c3be1
Chocolatey <chocolatey@realdimensions.net>
2021-10-26 18:43:15 +0000

    AU: 1 updated - chow-tape-model [skip ci] https://gist.github.com/4c58597cdc348acb837256cb29bc897d/6018c668dc2a7500004a402e91de250383151028

--- automatic/chow-tape-model/tools/chocolateyinstall.ps1
+++ automatic/chow-tape-model/tools/chocolateyinstall.ps1
@@ -13,11 +13,11 @@ Write-Host "$env:ChocolateyPackageName is going to be installed in '$installDir'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName  
   unzipLocation  = $installDir
-  url            = 'https://github.com/jatinchowdhury18/AnalogTapeModel/releases/download/v2.7.0/ChowTape-Win32-2.7.0.zip'
-  checksum       = '1F2D9BFA91D3BE4B3E1F5CEC7425A720F58BE1AE7A16FC3056071A037977B778'
+  url            = 'https://github.com/jatinchowdhury18/AnalogTapeModel/releases/download/v2.9.0/CHOWTapeModel-Win32-2.9.0.zip'
+  checksum       = '074d07a6bdcba8f92f235a9721fb6f1e2a103e214858bd840e0430c3ac669869'
   checksumType   = 'sha256'
-  url64          = 'https://github.com/jatinchowdhury18/AnalogTapeModel/releases/download/v2.7.0/ChowTape-Win64-2.7.0.zip'
-  checksum64     = '5E1753E971D8A834E0E7C345EDD41FC63F86EAF940376D7650D3D900D3C9F1BE'
+  url64          = 'https://github.com/jatinchowdhury18/AnalogTapeModel/releases/download/v2.9.0/CHOWTapeModel-Win64-2.9.0.zip'
+  checksum64     = '41a2474d0c85f8729746def508cefc3a35b13b948ba14df2099345aa32371403'
   checksumType64 = 'sha256'  
 }
 
