ae717e54c7e35538203fc36a4182bc27eada12e5
Scott Walters <me@scowalt.com>
2021-11-06 13:06:17 -0700

    cryptomator v1.6.3

--- manual/cryptomator/tools/chocolateyInstall.ps1
+++ manual/cryptomator/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'cryptomator'
   fileType               = 'msi'
-  url64bit               = 'https://github.com/cryptomator/cryptomator/releases/download/1.6.1/Cryptomator-1.6.1-x64.msi'
-  checksum64             = '444F1664C11D39B2E6FA682ECFB598C2760E714761752F700F67BE1FA427D9E6'
+  url64bit               = 'https://github.com/cryptomator/cryptomator/releases/download/1.6.3/Cryptomator-1.6.3-x64.msi'
+  checksum64             = 'DB43705ADFF25796464239854AF70A066668D21535874E1B494FB98EA12DFEFB'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /norestart'
   validExitCodes         = @(0)
