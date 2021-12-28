aa1dc2542ac7adceebaca747664f094657f7d474
Chocolatey <chocolatey@realdimensions.net>
2021-12-28 00:05:56 +0000

    AU: 1 updated - multi-gitter [skip ci] https://gist.github.com/4c58597cdc348acb837256cb29bc897d/473769e485097f4435c312c82935fdc9bc0f4036

--- automatic/multi-gitter/tools/chocolateyinstall.ps1
+++ automatic/multi-gitter/tools/chocolateyinstall.ps1
@@ -17,11 +17,11 @@ New-Item -ItemType Directory -Path $tempDir
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName  
   unzipLocation  = $tempDir
-  url            = 'https://github.com/lindell/multi-gitter/releases/download/v0.38.0/multi-gitter_0.38.0_Windows_i386.tar.gz'
-  checksum       = '916a56454574269e8ab4ca0725d6e06363b11cc1c464fc750b7caae4e6dba554'
+  url            = 'https://github.com/lindell/multi-gitter/releases/download/v0.38.2/multi-gitter_0.38.2_Windows_i386.tar.gz'
+  checksum       = '24bd41f993341c75e70729d11e570e1c74858801d704baed34ae82a67fd8dac1'
   checksumType   = 'sha256'
-  url64          = 'https://github.com/lindell/multi-gitter/releases/download/v0.38.0/multi-gitter_0.38.0_Windows_x86_64.tar.gz'
-  checksum64     = '1a640a72a3d131e838bd002b7adb57b50374b48b4d56e872c721aeeeafa747b0'
+  url64          = 'https://github.com/lindell/multi-gitter/releases/download/v0.38.2/multi-gitter_0.38.2_Windows_x86_64.tar.gz'
+  checksum64     = '853f5bc8d1a98f4893fb4209d0765b1de771bfa7955b8b6a5756968ba7615e5f'
   checksumType64 = 'sha256'  
 }
 
