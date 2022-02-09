8692ad3dec6e76c4dd437d841144064ace09896f
Silvan Kohler <silvankohler@protonmail.com>
2022-02-09 22:34:34 +0100

    Update update.ps1

--- automatic/trilium-notes/update.ps1
+++ automatic/trilium-notes/update.ps1
@@ -17,9 +17,9 @@ function global:au_GetLatest {
   $regex   = 'trilium-windows-x64-\d+.\d+.\d+.zip$'
   $sublink = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -Skip 0 -expand href #2
   $url = ($domain, $sublink) -join ''
-  $token = $url -split 'trilium-windows-x64-' | Select-Object -First 1 -Skip 1 #3
-  $version = $token -split '.zip' | Select-Object -Last 1 -Skip 1 #3
+  $token = $url -split 'trilium-windows-x64-' | Select-Object -First 1 -Skip 1
+  $version = $token -split '.zip' | Select-Object -Last 1 -Skip 1
   return @{ Version = $version; URL = $url }
 }
 
-update -ChecksumFor 64
+update -ChecksumFor 64 #-NoCheckChocoVersion
