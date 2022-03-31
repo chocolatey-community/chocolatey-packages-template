98a993551a9849fba9be6f07ef0d4bc70e88b565
scowalt <scowalt@users.noreply.github.com>
2022-03-31 04:11:47 +0000

    Cryptomator

--- packages/cryptomator/tools/chocolateyInstall.ps1
+++ packages/cryptomator/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 $ErrorActionPreference = 'Stop'
 
-$packageVersion = "1.6.7"
+$packageVersion = "1.6.8"
 
 $packageArgs = @{
   packageName            = 'cryptomator'
   fileType               = 'msi'
   url64bit               = "https://github.com/cryptomator/cryptomator/releases/download/$packageVersion/Cryptomator-$packageVersion-x64.msi"
-  checksum64 = 'e170c2fa1f47170efe853ff4f5b05763271ff335b6e0c50b976ca02d5bb67b79'
+  checksum64 = '6f0321d8f7eb9db13ad726354c87fa39b3d895e0895d4f05c6e38d3f584453f8'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /norestart'
   validExitCodes         = @(0)
@@ -19,4 +19,5 @@ if ($installLocation)  {
     Write-Host "$packageName installed to '$installLocation'"
 }
 else { Write-Warning "Can't find $PackageName install location" }
+
 
