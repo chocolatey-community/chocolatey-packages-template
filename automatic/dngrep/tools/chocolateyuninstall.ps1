3ecf562015d9e7a9b0f67682bf52964a7802c9ad
Your Name <you@example.com>
2022-08-05 17:00:19 -0400

    more updates

---
+++ automatic/dngrep/tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'dngrep'
+
+$installLocation = Get-AppInstallLocation "$packageName*"
+if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+
+rm $installLocation -recurse -force
+Write-Host "$packageName removed"
--- automatic/pandoc/update.ps1
+++ automatic/pandoc/update.ps1
@@ -4,10 +4,11 @@ $releases = 'https://github.com/jgm/pandoc/releases'
 
 function global:au_SearchReplace {
     @{
+
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
         }
     }
  }
@@ -16,6 +17,7 @@ function global:au_GetLatest {
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $url = $download_page.Links | where href -match '\.msi$' | % href | select -first 1
+    $url = 'https://github.com' * $url
     $version = $url -split '/' | select -Last 1 -Skip 1
 
     @( URL32 = $url; Version = $version )
