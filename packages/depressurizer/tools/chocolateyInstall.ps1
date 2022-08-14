17460c5a14ecfc9a3fcc1bc89c7687a769b1beb1
Scott Walters <me@scowalt.com>
2022-08-14 09:17:13 -0700

    DONE

--- packages/depressurizer/tools/chocolateyInstall.ps1
+++ packages/depressurizer/tools/chocolateyInstall.ps1
@@ -4,18 +4,16 @@ $packageName = 'depressurizer'
 $url = "https://github.com/Depressurizer/Depressurizer/releases/download/v5.2.0/Depressurizer-v5.2.0.exe"
 $checksum = '8254bdeae5cac5bf8b75f4a59f1d42cb4152961eb5170526ce483c03759375c5'
 
-$packageArgs = @{
-    packageName    = $packageName
-    unzipLocation  = $toolsDir
-    fileType       = 'exe'
-    url            = $url
-    softwareName   = 'Depressurizer*'
 
-    checksum       = $checksum
-    checksumType   = 'sha256'
+$destination = "$env:LOCALAPPDATA\programs\Depressurizer"
+mkdir $destination -Force
 
-    silentArgs     = '/S'
-    validExitCodes = @(0)
-}
+Get-ChocolateyWebFile `
+    -PackageName $packageName `
+    -FileFullPath "$destination\Depressurizer.exe" `
+    -ForceDownload `
+    -Url $url `
+    -Checksum $checksum `
+    -ChecksumType 'sha256' `
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyShortcut -ShortcutFilePath "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Depressuzizer.lnk" -TargetPath "$destination\Depressurizer.exe"
