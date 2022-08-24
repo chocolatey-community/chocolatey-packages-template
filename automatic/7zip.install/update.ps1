bc42c35cbe8c0007fe7008e31b2bfe625616e393
Brian Brito <brian.brito@ensemblehp.com>
2022-08-24 14:00:36 -0400

    added 7zip

---
+++ automatic/7zip.install/update.ps1
@@ -0,0 +1,25 @@
+. "$PSScriptRoot\..\7zip\update.ps1"
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
+
+$softwareNamePrefix = '7-zip'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameBase '7zip' }
+
+function global:au_AfterUpdate {
+  Update-ChangelogVersion -Version $Latest.Version
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\verification.txt" = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
+      "(?i)(64-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL64)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType)"
+      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+      "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
+    }
+  }
+}
+
+update -ChecksumFor none -NoCheckUrl
