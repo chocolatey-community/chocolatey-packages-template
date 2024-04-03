856a973805e805a4aa9ea9351c1ff7ea2d0e8579
Chocolatey <chocolatey@realdimensions.net>
2024-04-03 00:01:20 +0000

    AU: 1 updated - newrelic-infra [skip ci] https://gist.github.com/cbab2a407ba1e3def260bb8a9d3ec454/2918fe7a5e28b56ea2f7814888d34af19a0a96b6

--- automatic/newrelic-infra/tools/chocolateyinstall.ps1
+++ automatic/newrelic-infra/tools/chocolateyinstall.ps1
@@ -37,7 +37,7 @@ $packageName     = 'newrelic-infra'
 $softwareName    = 'newrelic-infra*'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://download.newrelic.com/infrastructure_agent/windows/newrelic-infra.1.51.0.msi'
+$url64      = 'https://download.newrelic.com/infrastructure_agent/windows/newrelic-infra.1.52.0.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -47,7 +47,7 @@ $packageArgs = @{
 
   softwareName  = $softwareName
 
-  checksum64     = '301ce357d3204e6dc261d613cc7f8fd59aa2418ab9f4bc139906d3cdf3c28dc4'
+  checksum64     = 'f09cce5a9dbb24729a039c8a61f466a396f0e0990b4ec2d0823c18aedd23cd29'
   checksumType64 = 'sha256'
 
   silentArgs    = "/qn $additionalArgs /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
